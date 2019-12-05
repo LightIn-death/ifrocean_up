aafcaa4e608c867e9d804251e85235540f27f683
lightin <breval2000@live.fr>
2019-12-05 16:51:08 +0100

    Mise en place difference admin benevole

--- pages/home.php
+++ pages/home.php
@@ -5,11 +5,25 @@ include_once "../components/HeaderFooter.php";
 include_once "../includes/sessionFonctions.php";
 pageHeader("Home");
 var_dump($_SESSION);
+if ($_SESSION["personne"]["admin"]) {
+
+    echo "\n tu est admin \n";
+
+} else if (!$_SESSION["personne"]["admin"]) {
+
+    echo "\ntu est un benevole \n";
+
+} else {
+
+    header('Location: /pages/connexion.php');
+}
+
+
 ?>
 
 
     <a href="../includes/LogOut.php">deconexion</a>
-
-
 <?php
+
+
 pageFooter();

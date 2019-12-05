1b237a9750066ca5665324590932e92e552497b1
lightin <breval2000@live.fr>
2019-12-05 16:39:07 +0100

    déconnexions

--- pages/home.php
+++ pages/home.php
@@ -4,9 +4,12 @@ session_start();
 include_once "../components/HeaderFooter.php";
 include_once "../includes/sessionFonctions.php";
 pageHeader("Home");
-
-
 var_dump($_SESSION);
+?>
 
 
+    <a href="../includes/LogOut.php">deconexion</a>
+
+
+<?php
 pageFooter();
